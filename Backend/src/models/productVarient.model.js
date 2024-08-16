ff6663ce872b0b401a5626afd6d62110e04d9f9c
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-16 17:36:48 +0530

    feat: Add product variant model and schema

---
+++ Backend/src/models/productVarient.model.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const productVarientSchema = new Schema(
+    {
+        productId: {
+            type: Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        size: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        stockQuantity: {
+            type: Map,
+            of: Number,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const ProductVarient = mongoose.model(
+    "ProductVarient",
+    productVarientSchema
+);
