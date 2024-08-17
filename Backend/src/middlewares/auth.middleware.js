8c315ea0f12a8b8104351bfe5831bd40efecc2bf
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-17 10:46:32 +0530

    feat: Add authentication middleware

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import { User } from "../models/index.js";
+import ApiError from "../utils/apiError.js";
+import { HTTP_UNAUTHORIZED } from "../httpStatusCode.js";
+import jwt from "jsonwebtoken";
+
+const verifyToken = asyncHandler(async (req, res, next) => {
+    try {
+        const token =
+            req.cookies?.authToken ||
+            req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            throw new ApiError(HTTP_UNAUTHORIZED, "Please authenticate");
+        }
+
+        const decodedTokenInfo = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decodedTokenInfo?._id).select(
+            "-password -refreshToken"
+        );
+        if (!user) {
+            throw new ApiError(HTTP_UNAUTHORIZED, "Invalid token");
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(HTTP_UNAUTHORIZED, "Please authenticate");
+    }
+});
+
+export default verifyToken;
