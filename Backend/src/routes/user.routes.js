b7109ea680e2c2a04761c6f14f20c59fbbec5f7c
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-18 01:14:23 +0530

    feat: Add user routes for API version 1

---
+++ Backend/src/routes/user.routes.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import {
+    registerUser,
+    verifyEmail,
+    resendVerificationEmail,
+    loginUser,
+    logoutUser,
+    changeCurrentPassword,
+    forgotPassword,
+    getUserProfile,
+    updateUserProfile,
+    updateUsername,
+    updateUserAddress,
+    resetPassword,
+} from "../controllers/user.controller.js";
+import verifyToken from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.post("/register", registerUser);
+router.post("/verify-email", verifyEmail);
+router.post("/resend-verification-email", verifyToken, resendVerificationEmail);
+router.post("/login", loginUser);
+router.post("/logout", verifyToken, logoutUser);
+router.post("/change-password", verifyToken, changeCurrentPassword);
+router.post("/forgot-password", forgotPassword);
+router.patch("/reset-password",  resetPassword);
+router.get("/get-user-profile", verifyToken, getUserProfile);
+router.patch("/update-profile", verifyToken, updateUserProfile);
+router.patch("/update-username", verifyToken, updateUsername);
+router.patch("/update-address", verifyToken, updateUserAddress);
+
+export default router;
