724fda129b5fea85ec1e61e36e0357562917b31c
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-18 01:13:31 +0530

    refactor: Update import statement for User model in auth middleware

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import { User } from "../models/index.js";
+import { User } from "../models/user.model.js";
 import ApiError from "../utils/apiError.js";
 import { HTTP_UNAUTHORIZED } from "../httpStatusCode.js";
 import jwt from "jsonwebtoken";
