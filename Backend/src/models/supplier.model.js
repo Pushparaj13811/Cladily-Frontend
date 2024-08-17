a707b21517a17265567d1d15b695bcb226f2b744
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-17 10:08:09 +0530

    feat: Add supplier model and schema

---
+++ Backend/src/models/supplier.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const supplierSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        contactNumeber: {
+            type: Number,
+            required: true,
+        },
+        contactEmail: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: Number,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Supplier = mongoose.model("Supplier", supplierSchema);
