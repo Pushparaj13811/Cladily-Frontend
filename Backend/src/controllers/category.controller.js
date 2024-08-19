06426d4583ae22860b4fa2fe1f49ea5101f6990a
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-19 14:46:09 +0530

    feat: Add category controller for creating, deleting, and retrieving categories

---
+++ Backend/src/controllers/category.controller.js
@@ -0,0 +1,92 @@
+import ApiResponse from "../utils/apiResponse.js";
+import ApiError from "../utils/apiError.js";
+import { Category } from "../models/category.model.js";
+import {
+    HTTP_BAD_REQUEST,
+    HTTP_INTERNAL_SERVER_ERROR,
+    HTTP_NOT_FOUND,
+    HTTP_OK,
+} from "../httpStatusCode.js";
+
+const createCategory = async (req, res, next) => {
+    // Get the category name and description from the request body
+    // Check if the category name and description are provided
+    // Check if the category name is unique
+    // Create the category
+    // Return the created category
+    // Handle any errors
+
+    const { name, description } = req.body;
+
+    if (!name || (!name.trim() && !description) || !description.trim()) {
+        throw new ApiError(
+            HTTP_BAD_REQUEST,
+            "Category name and description are required"
+        );
+    }
+
+    const categoryExists = await Category.findOne({ name });
+
+    if (categoryExists) {
+        throw new ApiError(HTTP_BAD_REQUEST, "Category already exists");
+    }
+
+    const category = new Category({
+        name,
+        description,
+    });
+
+    try {
+        const savedCategory = await category.save();
+        return res
+            .status(HTTP_OK)
+            .json(new ApiResponse(HTTP_OK, "Category created", savedCategory));
+    } catch (error) {
+        throw new ApiError(HTTP_INTERNAL_SERVER_ERROR, error.message);
+    }
+};
+const deleteCategory = async (req, res, next) => {
+    // Get the category id from the request params
+    // Check if the category exists
+    // Delete the category
+    // Return a success message
+    // Handle any errors
+
+    const { categoryId } = req.params;
+
+    try {
+        const category = await Category.findByIdAndDelete(categoryId);
+
+        if (!category) {
+            throw new ApiError(HTTP_NOT_FOUND, "Category not found");
+        }
+
+        return res
+            .status(HTTP_OK)
+            .json(new ApiResponse(HTTP_OK, "Category deleted"));
+    } catch (error) {
+        throw new ApiError(
+            error.statusCode || HTTP_INTERNAL_SERVER_ERROR,
+            error.message || "Internal server error"
+        );
+    }
+};
+const getCategories = async (req, res, next) => {
+    // Get all categories
+    // Return the categories
+    // Handle any errors
+
+    try {
+        const categories = await Category.find();
+        return res
+            .status(HTTP_OK)
+            .json(new ApiResponse(HTTP_OK, "Categories", categories));
+    } catch (error) {
+        throw new ApiError(
+            error.statusCode || HTTP_INTERNAL_SERVER_ERROR,
+            error.message || "Internal server error"
+        );
+    }
+};
+
+export { createCategory, deleteCategory, getCategories };
