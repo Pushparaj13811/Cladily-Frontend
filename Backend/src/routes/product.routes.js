da0614eb27c87045db899ca399a20ac42c26e9d1
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-21 12:59:10 +0530

    refactor: Update createProduct route to handle multiple image uploads

--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -7,12 +7,22 @@ import {
     fetchProductById,
 } from "../controllers/product.controller.js";
 import verifyToken from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.route("/").get(fetchAllProducts);
 router.route("/:id").get(fetchProductById);
-router.route("/create").post(verifyToken, createProduct);
+router.route("/create").post(
+    verifyToken,
+    upload.fields([
+        {
+            name: "image",
+            maxCount: 10,
+        },
+    ]),
+    createProduct
+);
 router.route("/update/:id").put(verifyToken, updateProduct);
 router.route("/delete/:id").delete(verifyToken, deleteProduct);
 
