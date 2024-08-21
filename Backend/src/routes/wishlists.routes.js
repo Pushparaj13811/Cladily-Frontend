f831ccb9ac5377ee1ba24db53bdb437b8df37110
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-21 08:25:17 +0530

    refactor: Add authentication middleware to wishlist routes

--- Backend/src/routes/wishlists.routes.js
+++ Backend/src/routes/wishlists.routes.js
@@ -3,11 +3,11 @@ import {
     addToWishlist,
     removeFromWishlist,
 } from "../controllers/wishlist.controller.js";
-
+import verifyToken from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.route("/").post(addToWishlist);
+router.route("/").post(verifyToken, addToWishlist);
 
-router.route("/:id").delete(removeFromWishlist);
+router.route("/:id").delete(verifyToken, removeFromWishlist);
 
 export default router;
