8903df05492253b0e2a0b705ec1f806f84a81e6a
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-21 13:29:45 +0530

    refactor: Update asyncHandler import in tokenGenerator.js

--- Backend/src/utils/tokenGenerator.js
+++ Backend/src/utils/tokenGenerator.js
@@ -1,5 +1,5 @@
 import crypto from "crypto";
-import asyncHandler from "./asyncHandler";
+import asyncHandler from "./asyncHandler.js";
 
 const generateGuestToken = asyncHandler(async () => {
     return crypto.randomBytes(32).toString("hex");
