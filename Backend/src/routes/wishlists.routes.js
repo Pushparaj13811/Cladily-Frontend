1d1506f17af8793f0b55578abac56b0c489bb48a
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-25 09:02:54 +0530

    refactor: Update wishlist routes to include getItemsFromWishlist endpoint

--- Backend/src/routes/wishlists.routes.js
+++ Backend/src/routes/wishlists.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import {
     addToWishlist,
+    getItemsFromWishlist,
     removeFromWishlist,
 } from "../controllers/wishlist.controller.js";
-import {verifyToken} from "../middlewares/auth.middleware.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.route("/").post(verifyToken, addToWishlist);
-
+router.route("/").get(verifyToken, getItemsFromWishlist);
+router.route("/add-items/").post(verifyToken, addToWishlist);
 router.route("/:id").delete(verifyToken, removeFromWishlist);
 
 export default router;
