d348fa8e4df2e9ed2a8d038d85b5c9060566fcc1
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-25 09:03:03 +0530

    refactor: Add coupon discount validation logic

--- Backend/src/controllers/discount.controller.js
+++ Backend/src/controllers/discount.controller.js
@@ -1,13 +1,65 @@
-import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/apiError.js";
-import ApiResponse from "../utils/apiResponse.js";
-import { Discount } from "../models/index.js";
-import {
-    HTTP_BAD_REQUEST,
-    HTTP_CREATED,
-    HTTP_UNAUTHORIZED,
-} from "../httpStatusCode.js";
+import { HTTP_BAD_REQUEST } from "../httpStatusCode.js";
+import { Coupon } from "../models/coupon.model.js";
 
-const discountValidation = async () => {
-    return { success: true, message: "Discount is valid" };
+const validateCouponDiscount = async (couponCode, totalAmount) => {
+    // Get the coupon code from the request body
+    // Check if the coupon code is provided
+    // Check if the coupon code is valid
+    // Get the total amount from the request body
+    // Check if the total amount is provided
+    // Check if the total amount is a number
+    // Check if the total amount is greater than 0
+    // Check if the minimum amount is greater than the total amount
+    // Check if the discount type is percentage
+    // Calculate the discount amount
+    // Return the discount amount
+    // Handle any errors
+
+    if (!couponCode) {
+        throw new ApiError(HTTP_BAD_REQUEST, "Coupon code is required");
+    }
+    if (!totalAmount) {
+        throw new ApiError(HTTP_BAD_REQUEST, "Total amount is required");
+    }
+    if (isNaN(totalAmount)) {
+        throw new ApiError(HTTP_BAD_REQUEST, "Total amount must be a number");
+    }
+    if (totalAmount <= 0) {
+        throw new ApiError(
+            HTTP_BAD_REQUEST,
+            "Total amount must be greater than 0"
+        );
+    }
+
+    const coupon = await Coupon.findOne({ code });
+
+    if (!coupon) {
+        throw new ApiError(HTTP_BAD_REQUEST, "Invalid coupon code");
+    }
+
+    if (coupon.expiryDate < new Date()) {
+        throw new ApiError(HTTP_BAD_REQUEST, "Coupon has expired");
+    }
+    if (!coupon.isActive) {
+        throw new ApiError(HTTP_BAD_REQUEST, "Coupon is not active");
+    }
+
+    if (coupon.minimumPurchaseAmount > totalAmount) {
+        throw new ApiError(
+            HTTP_BAD_REQUEST,
+            `Minimum purchase amount is ${coupon.minimumPurchaseAmount}`
+        );
+    }
+
+    if (coupon.discountType === "percentage") {
+        const discountAmount = (coupon.discountAmount / 100) * totalAmount;
+        return discountAmount;
+    }
+
+    return coupon.discountAmount;
 };
+
+
+
+export { validateCouponDiscount };
