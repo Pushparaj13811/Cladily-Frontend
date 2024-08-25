9404e7da4d4fe7b6f39ecb9da3ac20184e90e044
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-25 09:02:48 +0530

    refactor: Update user.routes.js to use router.route() for all endpoints

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -15,25 +15,29 @@ import {
     getUserAddress,
     deleteUserAddress,
 } from "../controllers/user.controller.js";
-import {verifyToken} from "../middlewares/auth.middleware.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 import createGuestSession from "../controllers/guest.controller.js";
 
 const router = Router();
 
-router.get("/",createGuestSession);
-router.post("/register", registerUser);
-router.post("/verify-email", verifyEmail);
-router.post("/resend-verification-email", verifyToken, resendVerificationEmail);
-router.post("/login", loginUser);
-router.post("/logout", verifyToken, logoutUser);
-router.post("/change-password", verifyToken, changeCurrentPassword);
-router.post("/forgot-password", forgotPassword);
-router.patch("/reset-password", resetPassword);
-router.get("/get-user-profile", verifyToken, getUserProfile);
-router.patch("/update-profile", verifyToken, updateUserProfile);
-router.patch("/update-username", verifyToken, updateUsername);
-router.patch("/update-address", verifyToken, updateUserAddress);
-router.get("/get-user-address", verifyToken, getUserAddress);
-router.delete("/delete-address/:addressId", verifyToken, deleteUserAddress);
+router.route("/").get(createGuestSession);
+router.route("/register").post(registerUser);
+router.route("/verify-email").post(verifyEmail);
+router
+    .route("/resend-verification-email")
+    .post(verifyToken, resendVerificationEmail);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyToken, logoutUser);
+router.route("/change-password").post(verifyToken, changeCurrentPassword);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password").patch(resetPassword);
+router.route("/get-user-profile").get(verifyToken, getUserProfile);
+router.route("/update-profile").patch(verifyToken, updateUserProfile);
+router.route("/update-username").patch(verifyToken, updateUsername);
+router.route("/update-address").patch(verifyToken, updateUserAddress);
+router.route("/get-user-address").get(verifyToken, getUserAddress);
+router
+    .route("/delete-address/:addressId")
+    .delete(verifyToken, deleteUserAddress);
 
 export default router;
