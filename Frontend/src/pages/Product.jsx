fd635b8d52da77b52bfa845cb42c4fa80dab9751
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-28 21:16:14 +0530

    refactor: Update Product component to implement lazy loading for images

--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -1,39 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import ProductCard from '../components/ProductCard';
 import { productNavLink } from '../constants/constants';
 import LinkSlider from '../components/LinkSlider';
-import { products } from "../constants/constants"
-
+import { products } from "../constants/constants";
 
 function Product() {
     const location = useLocation();
     const [currentPage, setCurrentPage] = useState(1);
-    const productsPerPage = 5;
+    const [loading, setLoading] = useState(false);
+    const [displayedProducts, setDisplayedProducts] = useState(products.slice(0, 10));
+    const productsPerPage = 10;
     const maxPagesToShow = 3;
 
     let showProductNav = "hidden";
-    let showProductInHomepage = "hidden"
-    let showProductInProductPage = "p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4"
-    let showPagination = "flex justify-center p-4"
+    let showProductInHomepage = "hidden";
+    let showProductInProductPage = "p-4 grid grid-cols-1 lg:mx-40  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-6";
+    let showPagination = "hidden sm:flex justify-center p-4";
+
     if (location.pathname === "/") {
-        showProductNav = "flex justify-center"
-        showProductInHomepage = "p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4"
-        showProductInProductPage = "hidden"
-        showPagination = "hidden"
-
+        showProductNav = "flex justify-center";
+        showProductInHomepage = "p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-4";
+        showProductInProductPage = "hidden";
+        showPagination = "hidden";
     }
-
-    const handlePageChange = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    };
 
     const totalPages = Math.ceil(products.length / productsPerPage);
 
-    const startIndex = (currentPage - 1) * productsPerPage;
-    const endIndex = startIndex + productsPerPage;
-    const productsToShow = products.slice(startIndex, endIndex);
-    const productsToShowInHomepage = products.slice(0, 5);
+    const handlePageScroll = (pageNumber) => {
+        setCurrentPage(pageNumber);
+        setDisplayedProducts(products.slice((0), pageNumber * productsPerPage));
+    };
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+        setDisplayedProducts(products.slice((pageNumber - 1) * productsPerPage, pageNumber * productsPerPage))
+
+    }
 
     const getPaginationButtons = () => {
         let startPage, endPage;
@@ -60,45 +62,70 @@ function Product() {
 
     const paginationButtons = getPaginationButtons();
 
+    // Infinite Scroll Handler
+    const loadMoreProducts = useCallback(() => {
+        if (loading) return;
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+
+        setLoading(true);
+        setTimeout(() => {
+            if (currentPage < totalPages) {
+                const nextPage = currentPage + 1;
+                handlePageScroll(nextPage);
+                setCurrentPage(nextPage);
+            }
+            setLoading(false);
+        }, 500);
+    }, [loading, currentPage, totalPages]);
+
+    useEffect(() => {
+        if (window.innerWidth <= 640) {
+            window.addEventListener('scroll', loadMoreProducts);
+        }
+        return () => {
+            window.removeEventListener('scroll', loadMoreProducts);
+        };
+    }, [loadMoreProducts]);
 
     return (
         <div>
             <h1 className="text-4xl font-bold font-heading text-secondary text-center p-4">Our Products</h1>
 
             {/* Product Navigation */}
-            <div className={` text-center text-secondary ${showProductNav}`}>
+            <div className={`text-center text-secondary ${showProductNav}`}>
                 <LinkSlider navLinks={productNavLink} />
             </div>
 
-            {/* Product Grid for homepage*/}
+            {/* Product Grid for homepage */}
             <div className={`${showProductInHomepage}`}>
-                {productsToShowInHomepage.map(product => (
+                {products.slice(0, 5).map(product => (
                     <ProductCard
                         key={product.id}
                         productName={product.name}
                         price={product.price}
                         imageUrls={product.imageUrls}
                         sizes={product.sizes}
+                        id={product.id}
                     />
                 ))}
             </div>
 
             {/* Product Grid for products page */}
-            {/* Product Grid */}
             <div className={`${showProductInProductPage}`}>
-                {productsToShow.map(product => (
+                {displayedProducts.map(product => (
                     <ProductCard
                         key={product.id}
                         productName={product.name}
                         price={product.price}
                         imageUrls={product.imageUrls}
                         sizes={product.sizes}
+                        id={product.id}
                     />
                 ))}
             </div>
 
             {/* Pagination Controls */}
-            <div className={showPagination}>
+            <div className={`${showPagination}`}>
                 <button
                     onClick={() => handlePageChange(currentPage - 1)}
                     disabled={currentPage === 1}
@@ -123,20 +150,19 @@ function Product() {
                     &raquo;
                 </button>
             </div>
+
             {/* Conditionally render the "View More" button */}
-            {
-                location.pathname === "/" && products.length > 5 && (
-                    <div className="text-center p-4">
-                        <Link
-                            to="/products"
-                            className="bg-secondary text-primary px-4 py-2 rounded"
-                        >
-                            View More
-                        </Link>
-                    </div>
-                )
-            }
-        </div >
+            {location.pathname === "/" && products.length > 5 && (
+                <div className="text-center p-4">
+                    <Link
+                        to="/products"
+                        className="bg-secondary text-primary px-4 py-2 rounded"
+                    >
+                        View More
+                    </Link>
+                </div>
+            )}
+        </div>
     );
 }
 
