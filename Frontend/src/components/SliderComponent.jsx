512aaf10d6577f3273dd20248488aff9b44bc116
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-28 21:15:53 +0530

    refactor: Update SliderComponent to adjust image height on extra large screens

--- Frontend/src/components/SliderComponent.jsx
+++ Frontend/src/components/SliderComponent.jsx
@@ -27,7 +27,7 @@ const SliderComponent = ({ imageUrls }) => {
                             <img
                                 src={url}
                                 alt={`Slide ${index}`}
-                                className="object-cover w-full h-80 md:h-96 mb-8"
+                                className="object-cover w-full h-80 md:h-96 mb-8 xl:h-[26rem]"
                             />
                         </div>
                     ))}
