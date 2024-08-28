417c31c80c8fe47e06e8a033677d9ded5bdbce4e
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-28 11:34:57 +0530

    refactor: Update ProductCard component to use secondary color for text

--- Frontend/src/components/ProductCard.jsx
+++ Frontend/src/components/ProductCard.jsx
@@ -12,15 +12,15 @@ function ProductCard({ productName, price, imageUrls, sizes, currency }) {
 
             {/* Product Details */}
             <div className="p-4 mt-5">
-                <h3 className="text-sm text-[#285c3f]">{productName}</h3>
-                <p className="text-sm font-body text-[#285c3f]">{currency + " " + price}</p>
+                <h3 className="text-sm text-secondary">{productName}</h3>
+                <p className="text-sm font-body text-secondary">{currency + " " + price}</p>
                 {/* Available Sizes */}
                 <div className="flex flex-wrap gap-2">
                     {sizes && sizes.map((size) => (
                         <Link
                             to={`/products/${size}`}
                             key={size}
-                            className="text-[#285c3f] hover:text-[#244b33] text-sm rounded-lg"
+                            className="text-secondary hover:text-tertery text-sm rounded-lg"
                         >
                             {size}
                         </Link>
