532235d38024cd323cdc99eb548d2a4f42b50c7f
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-30 15:43:29 +0530

    refactor: Update product creation route to handle dynamic fields and validate product data

--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -6,23 +6,14 @@ import {
     fetchAllProducts,
     fetchProductById,
 } from "../controllers/product.controller.js";
-import {verifyToken} from "../middlewares/auth.middleware.js";
-import { upload } from "../middlewares/multer.middleware.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
+import { handleDynamicFields,upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.route("/").get(fetchAllProducts);
 router.route("/:id").get(fetchProductById);
-router.route("/create").post(
-    verifyToken,
-    upload.fields([
-        {
-            name: "image",
-            maxCount: 10,
-        },
-    ]),
-    createProduct
-);
+router.route("/create").post(verifyToken,upload, handleDynamicFields, createProduct);
 router.route("/update/:id").put(verifyToken, updateProduct);
 router.route("/delete/:id").delete(verifyToken, deleteProduct);
 
